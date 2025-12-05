f23a9e1af7a7d6b350b7cffee4f569354a17a2f7
Shipon Roy <shiponworks@gmail.com>
2025-12-05 21:11:55 +0600

    Dist folder deleted

--- dist/js/script.js
+++
@@ -1,6 +0,0 @@
-const mobileBars = document.getElementById('Mobile_bar');
-const mobile = document.querySelector('ul');
-
-mobileBars.addEventListener('click', () => {
-  mobile.classList.toggle('hidden');
-});
